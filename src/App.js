51c025454348a00118d57e14bd1b05da9b75b0fa
sabinathapa375 <Sabina.thapa@greentick.com.np>
2023-12-27 11:31:41 +0545

    Controlled Form added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ControlledComponent=()=>{
+  const [inputValue, setInputValue] = useState('');
+
+  const [selectedOption, setselectedOption] = useState('option1');
+
+  const [isChecked, setisChecked] =  useState(false);
+  const handleChange=(event)=>{
+    setInputValue(event.target.value);
+  };
+
+  const handleOptionChange=(event)=>{
+    setselectedOption(event.target.value)
+  }
+
+  const handleCheck=(event)=>{
+    setisChecked(event.target.checked)
+  }
+
+  return(
+    
+      <form>
+
+        <label>
+          Input Value
+          <input type="text" value={inputValue} onChange={handleChange}/>
+        </label>
+        <p>Input Value : {inputValue}</p>
+
+
+        {/* For Dropdown */}
+        <label>
+          Select an option:
+            <select value={selectedOption} onChange={handleOptionChange}>
+              <option value="option1">Option1</option>
+              <option value="option2">Option2</option>
+              <option value="option3">Option3</option>
+            </select>
+        </label>
+        <p>Selected Option:{selectedOption}</p>
+
+        {/* For checkbox */}
+        <label htmlFor='color'>
+          <input type='checkbox' checked={isChecked} onChange={handleCheck}/>
+          Orange
+        </label>
+
+      </form>
+  )
 }
 
-export default App;
+export default ControlledComponent;
+
+
+ 
+  
